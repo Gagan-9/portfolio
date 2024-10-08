e5f2c2e4fe5da711570d1605aabf39159f81cbfd
Gagandeep Kaur <deepgagan.2309@gmail.com>
2024-10-08 23:47:54 +0530

    Skills.jsx updated

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,58 +1,65 @@
 import React from 'react';
 import skillsCSS from './../Skills/Skills.module.css';
+import cPlusPlusImg from './../../assets/c++.jpg';
+import javascriptImg from './../../assets/javascript.jpg';
+import reactImg from './../../assets/react.jpg';
+import htmlImg from './../../assets/html.jpg';
+import cssImg from './../../assets/css.jpg';
+import nodeImg from './../../assets/node.jpg';
+import mongoImg from './../../assets/mongo.jpg';
+import sqlImg from './../../assets/sql.jpg';
+import gitImg from './../../assets/git.jpg';
 
 function Skills() {
   return (
     <div id="skills" className={`${skillsCSS.SkillsWrapper} section`}>
       <h3 className="sectionheading"><i className="ri-shining-fill">Skills</i></h3>
       <div className={skillsCSS.skillsGrid}>
-
         {/* Individual skill cards */}
         <div className={skillsCSS.skillCard}>
           <h4>C++</h4>
-          <img src="src/assets/c++.jpg" alt="C++" className={skillsCSS.skillImage} />
+          <img src={cPlusPlusImg} alt="C++" className={skillsCSS.skillImage} />
         </div>
 
         <div className={skillsCSS.skillCard}>
           <h4>JavaScript</h4>
-          <img src="src/assets/javascript.jpg" alt="JavaScript" className={skillsCSS.skillImage} />
+          <img src={javascriptImg} alt="JavaScript" className={skillsCSS.skillImage} />
         </div>
 
         <div className={skillsCSS.skillCard}>
           <h4>React</h4>
-          <img src="src/assets/react.jpg" alt="React" className={skillsCSS.skillImage} />
+          <img src={reactImg} alt="React" className={skillsCSS.skillImage} />
         </div>
 
         <div className={skillsCSS.skillCard}>
           <h4>HTML5</h4>
-          <img src="src/assets/html.jpg" alt="HTML5" className={skillsCSS.skillImage} />
+          <img src={htmlImg} alt="HTML5" className={skillsCSS.skillImage} />
         </div>
 
         <div className={skillsCSS.skillCard}>
           <h4>CSS3</h4>
-          <img src="src/assets/css.jpg" alt="CSS3" className={skillsCSS.skillImage} />
+          <img src={cssImg} alt="CSS3" className={skillsCSS.skillImage} />
         </div>
 
         <div className={skillsCSS.skillCard}>
           <h4>Node.js</h4>
-          <img src="src/assets/node.jpg" alt="Node.js" className={skillsCSS.skillImage} />
+          <img src={nodeImg} alt="Node.js" className={skillsCSS.skillImage} />
         </div>
 
         <div className={skillsCSS.skillCard}>
           <h4>MongoDB</h4>
-          <img src="src/assets/mongo.jpg" alt="MongoDB" className={skillsCSS.skillImage} />
+          <img src={mongoImg} alt="MongoDB" className={skillsCSS.skillImage} />
         </div>
 
         <div className={skillsCSS.skillCard}>
           <h4>MySQL</h4>
-          <img src="src/assets/sql.jpg" alt="MySQL" className={skillsCSS.skillImage} />
+          <img src={sqlImg} alt="MySQL" className={skillsCSS.skillImage} />
         </div>
 
         <div className={skillsCSS.skillCard}>
           <h4>Git</h4>
-          <img src="src/assets/git.jpg" alt="Git" className={skillsCSS.skillImage} />
+          <img src={gitImg} alt="Git" className={skillsCSS.skillImage} />
         </div>
-
       </div>
     </div>
   );
