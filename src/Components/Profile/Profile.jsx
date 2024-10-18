418a30c39f121ec47703ebcbebc1f452d46e314f
Gagandeep Kaur <deepgagan.2309@gmail.com>
2024-10-18 23:17:22 +0530

    leetcode added

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import profileCSS from './../Profile/Profile.module.css';
 import HeroImg from '../../assets/Hero-img.jpg';
-// Remove the import for CV if you won't use it directly
-// import CV from '../../assets/CV.pdf';
-
 function Profile() {
   return (
     <div className={profileCSS.ProfileWrapper}>
@@ -18,7 +15,7 @@ function Profile() {
           <a href="mailto:deepgagan.2309@gmail.com" className={profileCSS.Icon}><i className="ri-mail-line"></i></a>
           <a href="https://linkedin.com/in/gagank9" className={profileCSS.Icon}><i className="ri-linkedin-box-line"></i></a>
           <a href="https://github.com/Gagan-9" className={profileCSS.Icon}><i className="ri-github-line"></i></a>
-          <a href="https://instagram.com/in/gagank.o9" className={profileCSS.Icon}><i className="ri-instagram-line"></i></a>
+          <a href="https://leetcode.com/Gagank9" className={profileCSS.Icon}><i className="ri-code-line"></i></a>
         </div>
         <a 
           href="https://drive.google.com/file/d/12pV3djR-oogT-Z1SZoHucj1EGZP3nzEr/view?usp=sharing"  
